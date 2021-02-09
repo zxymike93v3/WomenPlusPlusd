4bc07b7753551627424be1c569a4c8b77b882a42
zxymike93v3 <a70710521@sina.com>
2021-02-09 13:55:57 +0800

    add router

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,12 +1,46 @@
 import "./App.css";
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
 
 function App() {
   return (
     <div className="App">
-        <div className="container">
-            <LoginScreen />
-        </div>
+
+        {/*<div className="container">*/}
+        {/*    <LoginScreen />*/}
+        {/*</div>*/}
+
+        <Router>
+            <div>
+                {/*<nav>*/}
+                {/*    <ul>*/}
+                {/*        <li>*/}
+                {/*            <Link to="/">Home</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*            <Link to="/about">About</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*            <Link to="/users">Users</Link>*/}
+                {/*        </li>*/}
+                {/*    </ul>*/}
+                {/*</nav>*/}
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/login">
+                        <LoginScreen />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
