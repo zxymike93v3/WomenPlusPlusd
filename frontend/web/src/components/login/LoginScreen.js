513f7490b0aa3b5306fd31eb4518a87a150e699d
zxymike93v3 <a70710521@sina.com>
2021-02-21 09:45:17 +0800

    add axios and api call to login form

--- frontend/web/src/components/login/LoginScreen.js
+++ frontend/web/src/components/login/LoginScreen.js
@@ -1,34 +1,52 @@
 import "./LoginScreen.scss"
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams
-} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from 'react';
+
+// to do: move to shared state
+const baseURL = "http://edunity.azurewebsites.net/student/login";
 
 function LoginScreen(props) {
-    let match = useRouteMatch();
+    const history = useHistory();
 
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const logIn = () => {
+        if (username && password) {
+            axios.post(baseURL, {
+                email: username,
+                password: password
+            }).then((response) => {
+                console.log(response);
+                if (response.status === 200) {
+                    history.push("/home");
+                } else {
+                    alert('login was not successful :(')
+                }
+            });
+        } else {
+            alert('please fill in username and password')
+        }
+    }
     return (
         <div className="row entry-screen">
             <div className="col-lg-6">
                 <h1>Welcome to ExamPortal</h1>
                 <form>
                     <div className="mb-4">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <label htmlFor="user-email" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="user-email" onChange={e => setUserName(e.target.value)} />
                     </div>
                     <div className="mb-4">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <label htmlFor="user-password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="user-password" onChange={e => setPassword(e.target.value)} />
                     </div>
                     <div className="mb-4">
                         <Link to={`/forgot-password`} className="form-text">Forgot your password?</Link>
                     </div>
                     <div className="mb-4">
-                        <button type="button" className="btn btn-primary w-100">Log In</button>
+                        <button type="button" className="btn btn-primary w-100" onClick={logIn}>Log In</button>
                     </div>
                     <div className="mb-4">
                         <div className="form-text">Don’t have an account?</div>
