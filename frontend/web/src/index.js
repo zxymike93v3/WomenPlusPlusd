2ca407b274fa0d384839f26b19e45ad8f4a75d34
zxymike93v3 <a70710521@sina.com>
2021-05-15 23:36:01 +0800

    translation library applied and added french subtitles on dashboard to test

--- frontend/web/src/index.js
+++ frontend/web/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './scss/custom.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import userReducer from "./features/user";
+
+import axios from './shared/axios';
+import "./shared/i18n"
 
 const store = configureStore({
   reducer: {
@@ -14,11 +18,24 @@ const store = configureStore({
   },
 });
 
+const currentEmail = localStorage.getItem('currentEmail')
+if (currentEmail) {
+  axios
+  .get(`student/${currentEmail}`)
+  .then((response) => {
+    if (response.data.language === "French") localStorage.setItem("i18nextLng", "fr")
+    else if (response.data.language === "Arabic") localStorage.setItem("i18nextLng", "ar")
+    else localStorage.setItem("i18nextLng", "en")
+  })
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
