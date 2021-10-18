cfa50c866ac947cfcfc9609010666369355bafce
zxymike93v3 <a70710521@sina.com>
2021-10-18 20:06:30 +0800

    show question from api

--- frontend/web/src/components/mcq-screen/index.jsx
+++ frontend/web/src/components/mcq-screen/index.jsx
@@ -1,60 +1,23 @@
 import '../../scss/custom.scss';
 import './mcq-screen.scss';
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Question from "./question";
 import QuestionProgressBar from "./question-progress-bar";
-import {Button, Modal} from 'react-bootstrap';    
+import {Button, Modal} from 'react-bootstrap';
+import axios from "../../shared/axios";
 
 const MCQScreen = () => {
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0)
-  const [questions] = useState([
-    {
-      'question': 'What percentage of households globally lack access to basic hand-washing facilities?',
-      'questionNo': 1,
-      'answers': [
-        {
-          'label': 'A',
-          'text': '20%'
-        },
-        {
-          'label': 'B',
-          'text': '30%'
-        },
-        {
-          'label': 'C',
-          'text': '50%'
-        },
-        {
-          'label': 'D',
-          'text': '60%'
-        }
-      ]
-    },
-    {
-      'question': 'What is the basic definition of a noncommunicable disease?',
-      'questionNo': 2,
-      'answers': [
-        {
-          'label': 'A',
-          'text': 'A disease that is relatively uncommon'
-        },
-        {
-          'label': 'B',
-          'text': 'A disease for which there is no known cure'
-        },
-        {
-          'label': 'C',
-          'text': 'A disease that can be passed from person to person only through close and sustained contact'
-        },
-        {
-          'label': 'D',
-          'text': 'A disease that cannot be passed from person to person'
-        }
-      ]
-    }
-  ])
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    axios.get('exam-questions').then((response) => {
+      setQuestions([response.data[0]]);
+    });
+  }, []);
+
   const [chosenAnswers, setChosenAnswers] = useState([]);
-  const [currentChosenAnswer, setCurrentChosenAnswer] = useState('');
+  const [currentChosenAnswer, setCurrentChosenAnswer] = useState();
 
   const chooseAnswerHandler = (label) => {
     setCurrentChosenAnswer(label);
@@ -77,6 +40,9 @@ const MCQScreen = () => {
   const handleShowModal = () => setShowSubmitModal(true);
 
   const showSubmitScreen = () => {
+    setChosenAnswers((prevState) => {
+      return [...prevState, currentChosenAnswer];
+    });
     handleShowModal();
   }
 
@@ -85,17 +51,21 @@ const MCQScreen = () => {
       <div className='container'>
         <div className='row'>
           <div className='col-12'>
-            <div className='question-wrapper'>
-              <QuestionProgressBar
-                currentQuestionNumber={currentQuestionNumber}
-                questionsNumber={questions.length}
-              />
-              <Question
-                questionData={questions[currentQuestionNumber]}
-                chooseAnswerHandler={chooseAnswerHandler}
-                currentChosenAnswer={currentChosenAnswer}
-              />
-            </div>
+            {
+              questions.length > 0 &&
+              <div className='question-wrapper'>
+                <QuestionProgressBar
+                    currentQuestionNumber={currentQuestionNumber}
+                    questionsNumber={questions.length}
+                />
+                <Question
+                    questionData={questions[currentQuestionNumber]}
+                    currentQuestionNumber={currentQuestionNumber}
+                    chooseAnswerHandler={chooseAnswerHandler}
+                    currentChosenAnswer={currentChosenAnswer}
+                />
+              </div>
+            }
           </div>
         </div>
         <div className='row'>
--- frontend/web/src/components/mcq-screen/question/index.jsx
+++ frontend/web/src/components/mcq-screen/question/index.jsx
@@ -2,30 +2,23 @@ import './question.scss';
 import React from "react";
 
 const Question = (props) => {
-  // const [chosenAnswer, setChosenAnswer] = useState();
 
   const pickAnswer = (event) => {
     let label = event.target.id ? event.target.id : event.target.parentElement.id;
-
     props.chooseAnswerHandler(label);
-
-    // setChosenAnswer((prevLabel) => {
-    //   return label;
-    // });
-    // console.log(chosenAnswer)
   };
 
   return (
     <div className='mcq-screen-question'>
       <h4>
-        {props.questionData.questionNo}. {props.questionData.question}
+          {props.currentQuestionNumber}. {props.questionData.question_content}
       </h4>
       <div className='answers-list'>
         {
-          props.questionData.answers.map((answer) => (
-            <div key={answer.label} id={answer.label} className={`answer ${props.currentChosenAnswer === answer.label ? 'active' : ''}`} onClick={pickAnswer}>
-              <span className='answer-label'>{answer.label}</span>
-              <span className='answer-text'>{answer.text}</span>
+          props.questionData.possible_answers.map((answer, index) => (
+            <div key={`answer-${answer}`} id={index} className={`answer ${parseInt(props.currentChosenAnswer) === parseInt(index) ? 'active' : ''}`} onClick={pickAnswer}>
+                <span className='answer-label'>{index}</span>
+              <span className='answer-text'>{answer}</span>
             </div>
           ))
         }
