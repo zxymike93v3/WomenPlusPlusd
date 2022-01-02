3d840bf25bdf964e7e307aae2b173ce0b4c4e556
zxymike93v3 <a70710521@sina.com>
2022-01-02 22:00:33 +0800

    anti cheating dynamic  modal and retry

--- frontend/web/src/components/mcq-screen/index.jsx
+++ frontend/web/src/components/mcq-screen/index.jsx
@@ -3,22 +3,52 @@ import "./mcq-screen.scss";
 import React, { useEffect, useState } from "react";
 import Question from "./question";
 import QuestionProgressBar from "./question-progress-bar";
-import { Button, Modal } from "react-bootstrap";
 import axios from "../../shared/axios";
 import { useHistory, useLocation } from "react-router";
-import Timer from './timer'
+import Timer from "./timer";
+import McqModal from "./mcq-modal";
 
 const MCQScreen = () => {
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [examData, setExamData] = useState([]);
+  // eslint-disable-next-line
+  const [isCheating, setIsCheating] = useState(false);
+  const [modalTitle, setModalTitle] = useState(
+    "You have completed your MCQ exam!"
+  );
+  const [modalMessage, setModalMessage] = useState(
+    "All your answers have been saved (even when you were offline). Make sure you have internet connection and submit your exam to finish."
+  );
+  const [modalButton, setModalButton] = useState("Submit exam");
 
   const location = useLocation();
   const history = useHistory();
   const { id } = location.examSet;
   const examId = localStorage.getItem("examId");
 
+  const cheatingModal = () => {
+    setIsCheating(true);
+    setModalTitle("we detected that you cheated");
+    setModalMessage(
+      "Asegurate de submit tu examen con conexion a internet, en caso de no hacerlo tu nota sera un 0"
+    );
+    handleShowModal();
+  };
+  const onFocus = () => {
+    cheatingModal();
+  };
+
+  const visibilityChange = () => {
+    if (window.visibilityState !== "visible") {
+      cheatingModal();
+    }
+  };
+
   useEffect(() => {
+    window.addEventListener("focus", onFocus);
+    window.addEventListener("visibilitychange", visibilityChange);
+    if (isCheating) handleShowModal();
     axios.get(`exam-set/${id}/exam-questions`).then((response) => {
       const currentExamSet = response.data;
       console.log(response, "response");
@@ -26,7 +56,10 @@ const MCQScreen = () => {
       setQuestions(currentExamSet);
       setExamData(currentExamSet);
     });
-  }, []);
+    return () => {
+      window.removeEventListener("visibilitychange", visibilityChange);
+    };
+  }, [isCheating]);
 
   // eslint-disable-next-line
   const [chosenAnswers, setChosenAnswers] = useState([]);
@@ -60,9 +93,17 @@ const MCQScreen = () => {
   // submit questions on handleclosemodal
   const handleCloseModal = (e) => {
     console.log(chosenAnswers, "chosenAnsers ----------");
-    axios.post(`exam/${examId}/student-answers`, chosenAnswers);
-    history.push("/home");
-    setShowSubmitModal(false);
+    axios
+      .post(`exam/${examId}/student-answers`, chosenAnswers)
+      .then(() => {
+        history.push("/home");
+        setShowSubmitModal(false);
+      })
+
+      .catch((error) => {
+        setModalButton("Retry");
+        return Promise.reject(error);
+      });
   };
   const handleShowModal = () => {
     setShowSubmitModal(true);
@@ -81,7 +122,6 @@ const MCQScreen = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            
             {questions.length > 0 && (
               <div className="question-wrapper">
                 <Timer id={id}></Timer>
@@ -122,23 +162,13 @@ const MCQScreen = () => {
           </div>
         </div>
       </div>
-
-      <Modal show={showSubmitModal}>
-        <Modal.Header>
-          <Modal.Title>You have completed your MCQ exam!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          All your answers have been saved (even when you were offline). Make
-          sure you have internet connection and submit your exam to finish.
-        </Modal.Body>
-        {/* {chosenAnswers} */}
-        <Modal.Footer>
-          {/* TODO: send data to backend */}
-          <Button variant="primary" onClick={handleCloseModal}>
-            Submit exam
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <McqModal
+        showSubmitModal={showSubmitModal}
+        handleCloseModal={handleCloseModal}
+        modalTitle={modalTitle}
+        modalMessage={modalMessage}
+        modalButton={modalButton}
+      ></McqModal>
     </div>
   );
 };
