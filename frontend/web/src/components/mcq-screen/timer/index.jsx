1606c1464428269af512e3862187f723645b79a0
zxymike93v3 <a70710521@sina.com>
2022-01-12 05:15:30 +0800

    add submission of exam when time reaches 0

--- frontend/web/src/components/mcq-screen/timer/index.jsx
+++ frontend/web/src/components/mcq-screen/timer/index.jsx
@@ -1,50 +1,53 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import axios from '../../../shared/axios'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "../../../shared/axios";
 
-const Timer = ({id}) => {
-    const [ minutes, setMinutes ] = useState(0);
-    const [ seconds, setSeconds ] = useState(0);
+const Timer = ({ id, setIsTime }) => {
+  const [minutes, setMinutes] = useState(60);
+  const [seconds, setSeconds] = useState(0);
 
-    const currentEmail = localStorage.getItem("currentEmail");
+  const currentEmail = localStorage.getItem("currentEmail");
 
-    useEffect(()=>{
+  useEffect(() => {
     let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            } 
-        }, 1000)
-        return ()=> {
-            clearInterval(myInterval);
-          };
-    });
-    useEffect(() => {
-        axios.get(`student/${currentEmail}/current-exams`).then((response) => {
-        const currentExamSet = response.data.filter((el) => {
-            return el.exam_set_id === id;
-        });
-        console.log(response, "response student");
-        console.log(currentExamSet, 'minutess')
-        setMinutes(currentExamSet[0].duration_in_minutes);
-        })
-    }, [])
-
-    return (
-        <div>
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h6> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h6> 
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          setIsTime(true);
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
         }
-        </div>
-    )
-}
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+  useEffect(() => {
+    axios.get(`student/${currentEmail}/current-exams`).then((response) => {
+      const currentExamSet = response.data.filter((el) => {
+        return el.exam_set_id === id;
+      });
+      console.log(response, "response student");
+      console.log(currentExamSet, "minutess");
+      setMinutes(currentExamSet[0].duration_in_minutes);
+    });
+  }, []);
+
+  return (
+    <div>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h6>
+          {" "}
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h6>
+      )}
+    </div>
+  );
+};
 
 export default Timer;
