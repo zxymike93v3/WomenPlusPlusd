c713534a4c01da1f4a52eeb9408d9b092d0f4357
zxymike93v3 <a70710521@sina.com>
2022-04-10 17:53:14 +0800

    Add REST endpoint to get past exams of a student.

--- backend/app.py
+++ backend/app.py
@@ -273,6 +273,24 @@ def get_current_exams_of_a_student(query_email):
     all_exams_of_a_student_response = exam_handler.handle_get_all_objects_with_text_command(text_command)
     return all_exams_of_a_student_response
 
+@app.route('/student/<query_email>/past-exams')
+def get_past_exams_of_a_student(query_email):
+    student_response = student_handler.handle_get_first_object_by_attribute(email=query_email)
+    if student_response.status_code != 200:
+        # there is some error while getting student info, so we return the error itself
+        return student_response
+
+    # there is no error when getting student info,
+    # but we need to convert from reponse data to dict
+    student_data_dict = json.loads(student_response.get_data())
+    student_id = student_data_dict.get('id')
+    # we create SQLAlchemy text command of exams which was already taken
+    current_time = datetime.now()
+    text_command = text('student_id={} and taken_at is not null'.format(student_id))
+    all_exams_of_a_student_response = exam_handler.handle_get_all_objects_with_text_command(text_command)
+    return all_exams_of_a_student_response
+
+
 @app.route('/student/login', methods=['POST'])
 def student_login():
     try:
